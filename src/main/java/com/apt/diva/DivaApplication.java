b6991e8c4e30ada37b4eb773e3e68fdcc36187cd
spacedivver <142153611+spacedivver@users.noreply.github.com>
2025-01-08 22:10:34 +0900

    first commit

---
+++ src/main/java/com/apt/diva/DivaApplication.java
@@ -0,0 +1,13 @@
+package com.apt.diva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apt/diva/DivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apt.diva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
