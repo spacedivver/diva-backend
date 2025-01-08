faec952944f45aaa9e5482fdb9542b5b65f10eb5
sanghyuk <ish775800@gmail.com>
2025-01-08 22:23:57 +0900

    docs: security 로그인 제거, .gitignore 수정

--- src/main/java/com/apt/diva/DivaApplication.java
+++ src/main/java/com/apt/diva/DivaApplication.java
@@ -2,12 +2,13 @@ package com.apt.diva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DivaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DivaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DivaApplication.class, args);
+    }
 
 }
