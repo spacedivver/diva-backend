cffcc4a5349966cdc9dbcbaf9cc32039bbb4d58a
spacedivver <142153611+spacedivver@users.noreply.github.com>
2025-01-22 23:07:13 +0900

    feat: macroeconomic 코드 작성

---
+++ src/main/java/com/apt/diva/macroeconomics/domain/response/MacroeconomicsResponse.java
@@ -0,0 +1,12 @@
+package com.apt.diva.macroeconomics.domain.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MacroeconomicsResponse {
+
+    private String content;
+
+}
