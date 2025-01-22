8404383632387f9ae1cacd1b5c531f92cb8d896b
spacedivver <142153611+spacedivver@users.noreply.github.com>
2025-01-22 23:16:21 +0900

    feat: financial db 컬럼 종류 변경

--- src/main/java/com/apt/diva/financial/domain/response/FinancialResponse.java
+++ src/main/java/com/apt/diva/financial/domain/response/FinancialResponse.java
@@ -7,21 +7,6 @@ import lombok.Data;
 @Builder
 public class FinancialResponse {
 
-    private String stockCode;
-    private String currentAssets;
-    private String nonCurrentAssets;
-    private String totalAssets;
-    private String currentLiabilities;
-    private String nonCurrentLiabilities;
-    private String totalLiabilities;
-    private String capitalStock;
-    private String retainedEarnings;
-    private String totalEquity;
-    private String revenue;
-    private String operatingIncome;
-    private String incomeBeforeTaxes;
-    private String netIncome;
-    private String netIncomeLess;
-    private String comprehensiveIncome;
+    private String content;
 
 }
