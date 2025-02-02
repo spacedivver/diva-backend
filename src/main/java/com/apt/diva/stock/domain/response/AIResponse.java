911b13e6445384b7926c481399e286e2f0ef3d0a
spacedivver <142153611+spacedivver@users.noreply.github.com>
2025-02-03 00:04:11 +0900

    fix: 컬럼명 수정

--- src/main/java/com/apt/diva/stock/domain/response/AIResponse.java
+++ src/main/java/com/apt/diva/stock/domain/response/AIResponse.java
@@ -1,5 +1,6 @@
 package com.apt.diva.stock.domain.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AIResponse {
-    private Long analysis_result_id;
+
+//    @JsonProperty("analysis_result_id") // JSON 필드명과 매핑
+    private Long analysisResultId;
+
 }
