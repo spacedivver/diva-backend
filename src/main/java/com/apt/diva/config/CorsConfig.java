396dfbc3ae359904d4d545b137d3951fd61cbbd1
spacedivver <142153611+spacedivver@users.noreply.github.com>
2025-02-05 02:29:23 +0900

    fix: corsConfig 리팩토링

--- src/main/java/com/apt/diva/config/CorsConfig.java
+++ src/main/java/com/apt/diva/config/CorsConfig.java
@@ -1,6 +1,6 @@
 package com.apt.diva.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,15 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer(){
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST");
-            }
-        };
+    @Value("${server-url}")
+    private String serverUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(serverUrl)
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+
 }
